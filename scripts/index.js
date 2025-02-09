cea18238f13b9125c06dd0fc294e10411e435d41
balwan coding <balwansinghrajput1708@gmail.com>
2025-02-09 19:54:30 +0530

    nav bar change

--- scripts/index.js
+++ scripts/index.js
@@ -104,3 +104,11 @@ function loadBagItemObjects() {
     }
   });
 }
+
+function openSidebar() {
+  document.getElementById("sidebar").style.left = "0";
+}
+
+function closeSidebar() {
+  document.getElementById("sidebar").style.left = "-250px";
+}
